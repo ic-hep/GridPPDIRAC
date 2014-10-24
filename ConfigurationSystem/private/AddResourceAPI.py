73b184f7aee40f79cff48e4cb3fa079c0007a48e
Alexander Richards <a.richards@imperial.ac.uk>
2014-10-24 14:54:01 +0100

    finishing off the API

--- ConfigurationSystem/private/AddResourceAPI.py
+++ ConfigurationSystem/private/AddResourceAPI.py
@@ -2,11 +2,13 @@
 """
 API for adding resources to CS
 """
-from DIRAC import gLogger, gConfig, S_OK, S_ERROR, exit as DIRACExit
+import re
+from DIRAC import gLogger, gConfig, S_OK, S_ERROR
 from DIRAC.ConfigurationSystem.Client.Utilities import (getGridCEs,
                                                         getSiteUpdates,
                                                         getCEsFromCS,
-                                                        getGridSRMs
+                                                        getGridSRMs,
+                                                        getSRMUpdates
                                                         )
 from DIRAC.Core.Utilities.SitesDIRACGOCDBmapping import getDIRACSiteName
 from DIRAC.ConfigurationSystem.Client.CSAPI import CSAPI
@@ -16,9 +18,24 @@ from DIRAC.Core.Utilities.Pfn import pfnparse
 from DIRAC.ConfigurationSystem.Client.Helpers.Registry import getVOs
 
 
-def checkUnusedCEs(vo, domain, country_default='xx'):
+def checkUnusedCEs(vo, domain, country_default='xx',
+                   diracSiteTemplate='{domain}.{site}.{country}'):
     '''
     Check for unused CEs and add them where possible
+
+    vo                - The VO
+    domain            - The Grid domain e.g. LCG
+    country_default   - the default country code to use to substitute into
+                        the diracSite name
+    diracSiteTemplate - The template from which the DIRAC site name is
+                        generated:
+                        Can use substitutions:
+                              {domain}        - The Grid domain e.g. LCG,
+                              {site}          - The site name
+                              {country}       - The country code e.g. uk,
+                                                defaulting to country_default
+                                                if it cannot be determined
+                                                automatically
     '''
     ceBdiiDict = None
     gLogger.notice('looking for new computing resources '
@@ -74,7 +91,10 @@ def checkUnusedCEs(vo, domain, country_default='xx'):
             country = country_default
         result = getDIRACSiteName(site)
         if not result['OK']:
-            diracSite = "%s.%s.%s" % (domain, site, country)
+            #diracSite = "%s.%s.%s" % (domain, site, country)
+            diracSite = diracSiteTemplate.format(domain=domain,
+                                                 site=site,
+                                                 country=country)
             gLogger.notice('The site %s is not yet in the CS, adding it as %s'
                            % (site, diracSite))
             diracSites = [diracSite]
@@ -130,7 +150,7 @@ def addSite(diracSiteName, gridSiteName, *ces):
 
     try:
         diracGridType, _, _ = diracSiteName.split('.')
-    except:
+    except ValueError:
         gLogger.error("The DIRACSiteName should be of the form "
                       "GRID.LOCATION.COUNTRY for example LCG.CERN.ch")
         return S_ERROR('DIRACSiteName wrong format')
@@ -263,8 +283,7 @@ def updateSites(vo, ceBdiiDict):
         gLogger.error('Failed to get site updates', result['Message'])
         return S_ERROR('Failed to get site updates')
     changeSet = result['Value']
-    updateCS(changeSet)
-    return S_OK()
+    return updateCS(changeSet)
 
 
 def updateCS(changeSet):
@@ -298,9 +317,18 @@ def updateCS(changeSet):
     return S_OK()
 
 
-def checkUnusedSEs(vo):
+def checkUnusedSEs(vo, diracSENameTemplate='{DIRACSiteName}-disk'):
     '''
     Check for unused SEs
+
+    vo                  - The VO
+    diracSENameTemplate - The template from which the DIRAC SE name is
+                          generated.
+                          Can use substitutions:
+                              {domain}        - The Grid domain e.g. LCG,
+                              {DIRACSiteName} - The DIRAC site name,
+                              {country}       - The country code e.g. uk,
+                              {gridSE}        - The Grid SE name
     '''
     result = getGridSRMs(vo, unUsed=True)
     if not result['OK']:
@@ -334,89 +362,101 @@ def checkUnusedSEs(vo):
                                % site)
                 continue
             diracSites = result['Value']
-            yn = raw_input('\nDo you want to add new SRM SE %s at site(s) %s ? default yes [yes|no]: ' % (gridSE, str(diracSites)))
-            if not yn or yn.lower().startswith('y'):
-                if len(diracSites) > 1:
-                    prompt = 'Which DIRAC site the new SE should be attached to ?'
-                    for i, s in enumerate(diracSites):
-                        prompt += '\n[%d] %s' % (i, s)
-                    prompt += '\nEnter your choice number: '
-                    inp = raw_input(prompt)
-                    try:
-                        ind = int(inp)
-                    except:
-                        gLogger.notice('Can not interpret your choice: %s, try again later' % inp)
-                        continue
-                    diracSite = diracSites[ind]
-                else:
-                    diracSite = diracSites[0]
+            ## here
+            if len(diracSites) > 1:
+                gLogger.notice('Can not determine to which DIRAC site the '
+                               'new SE should be attached:')
+                gLogger.notice(str(diracSites.values()))
+                gLogger.notice('Please run the dirac-admin-add-resources '
+                               'command line tool to decide interactively')
+                continue
+            diracSite = diracSites[0]
 
-                domain, siteName, country = diracSite.split('.')
-                recName = '%s-disk' % siteName
-                inp = raw_input('Give a DIRAC name to the grid SE %s, default %s : ' % (gridSE, recName))
-                diracSEName = inp
-                if not inp:
-                    diracSEName = recName
+            ## here
+            domain, siteName, country = diracSite.split('.')
+            diracSEName = diracSENameTemplate.format(domain=domain,
+                                                     DIRACSiteName=siteName,
+                                                     country=country,
+                                                     gridSE=gridSE)
+            gLogger.notice('Grid SE %s will get the DIRAC name %s'
+                           % (gridSE, diracSEName))
 
-                gLogger.notice('Adding new SE %s at site %s' % (diracSEName, diracSite))
-                seSection = cfgPath('/Resources/StorageElements', diracSEName)
-                changeSet.add((seSection, 'BackendType', seDict.get('GlueSEImplementationName', 'Unknown')))
-                changeSet.add((seSection, 'Description', seDict.get('GlueSEName', 'Unknown')))
-                bdiiVOs = set([re.sub('^VO:', '', rule) for rule in srmDict.get('GlueServiceAccessControlBaseRule', [])])
-                seVOs = csVOs.intersection(bdiiVOs)
-                changeSet.add((seSection, 'VO', ','.join(seVOs)))
-                accessSection = cfgPath(seSection, 'AccessProtocol.1')
-                changeSet.add((accessSection, 'Protocol', 'srm'))
-                changeSet.add((accessSection, 'ProtocolName', 'SRM2'))
-                endPoint = srmDict.get('GlueServiceEndpoint', '')
-                result = pfnparse(endPoint)
-                if not result['OK']:
-                    gLogger.error('Can not get the SRM service end point. Skipping ...')
-                    continue
-                host = result['Value']['Host']
-                port = result['Value']['Port']
-                changeSet.add((accessSection, 'Host', host))
-                changeSet.add((accessSection, 'Port', port))
-                changeSet.add((accessSection, 'Access', 'remote'))
-                # Try to guess the Path
-                domain = '.'.join(host.split('.')[-2:])
-                path = '/dpm/%s/home' % domain
-                changeSet.add((accessSection, 'Path', path))
-                changeSet.add((accessSection, 'SpaceToken', ''))
-                changeSet.add((accessSection, 'WSUrl', '/srm/managerv2?SFN='))
+            gLogger.notice('Adding new SE %s at site %s'
+                           % (diracSEName, diracSite))
+            seSection = cfgPath('/Resources/StorageElements', diracSEName)
+            changeSet.add((seSection, 'BackendType',
+                           seDict.get('GlueSEImplementationName', 'Unknown')))
+            changeSet.add((seSection, 'Description',
+                           seDict.get('GlueSEName', 'Unknown')))
+            bdiiVOs = set([re.sub('^VO:', '', rule) for rule in
+                           srmDict.get('GlueServiceAccessControlBaseRule',
+                                       []
+                                       )
+                           ])
+            seVOs = csVOs.intersection(bdiiVOs)
+            changeSet.add((seSection, 'VO', ','.join(seVOs)))
+            accessSection = cfgPath(seSection, 'AccessProtocol.1')
+            changeSet.add((accessSection, 'Protocol', 'srm'))
+            changeSet.add((accessSection, 'ProtocolName', 'SRM2'))
+            endPoint = srmDict.get('GlueServiceEndpoint', '')
+            result = pfnparse(endPoint)
+            if not result['OK']:
+                gLogger.error('Can not get the SRM service end point. '
+                              'Skipping ...')
+                continue
+            host = result['Value']['Host']
+            port = result['Value']['Port']
+            changeSet.add((accessSection, 'Host', host))
+            changeSet.add((accessSection, 'Port', port))
+            changeSet.add((accessSection, 'Access', 'remote'))
+            # Try to guess the Path
+            domain = '.'.join(host.split('.')[-2:])
+            path = '/dpm/%s/home' % domain
+            changeSet.add((accessSection, 'Path', path))
+            changeSet.add((accessSection, 'SpaceToken', ''))
+            changeSet.add((accessSection, 'WSUrl', '/srm/managerv2?SFN='))
 
-                gLogger.notice('SE %s will be added with the following parameters')
-                changeList = list(changeSet)
-                changeList.sort()
-                for entry in changeList:
-                    gLogger.notice(entry)
-                yn = raw_input('Do you want to add new SE %s ? default yes [yes|no]: ' % diracSEName)
-                if not yn or yn.lower().startswith('y'):
-                    changeSetFull = changeSetFull.union(changeSet)
+            gLogger.notice('SE %s will be added with the following parameters')
+            changeList = list(changeSet)
+            changeList.sort()
+            for entry in changeList:
+                gLogger.notice(entry)
+            changeSetFull = changeSetFull.union(changeSet)
 
     if changeSetFull:
         csAPI = CSAPI()
         csAPI.initialize()
         result = csAPI.downloadCSData()
         if not result['OK']:
-            gLogger.error('Failed to initialize CSAPI object', result['Message'])
-            DIRACExit(-1)
+            gLogger.error('Failed to initialize CSAPI object',
+                          result['Message'])
+            return S_ERROR('Failed to initialize CSAPI object')
         changeList = list(changeSetFull)
         changeList.sort()
         for section, option, value in changeList:
             csAPI.setOption(cfgPath(section, option), value)
 
-        yn = raw_input('New SE data is accumulated\n Do you want to commit changes to CS ? default yes [yes|no]: ')
-        if not yn or yn.lower().startswith('y'):
-            result = csAPI.commit()
-            if not result['OK']:
-                gLogger.error("Error while commit to CS", result['Message'])
-            else:
-                gLogger.notice("Successfully committed %d changes to CS"
-                               % len(changeSetFull))
+        result = csAPI.commit()
+        if not result['OK']:
+            gLogger.error("Error while commit to CS", result['Message'])
+            return S_ERROR("Error while commit to CS")
+
+        gLogger.notice("Successfully committed %d changes to CS"
+                       % len(changeSetFull))
 
     return S_OK()
 
+
+def updateSEs(vo):
+    '''
+    update SEs
+    '''
+    result = getSRMUpdates(vo)
+    if not result['OK']:
+        gLogger.error('Failed to get SRM updates', result['Message'])
+        return S_ERROR('Failed to get SRM updates')
+    changeSet = result['Value']
+    return updateCS(changeSet)
 
 if __name__ == '__main__':
     import sys
@@ -434,11 +474,22 @@ if __name__ == '__main__':
 
     result = checkUnusedCEs(options.vo, options.domain)
     if not result['OK']:
-        gLogger.error("Error while running check for Unused CEs",
+        gLogger.error("Error while running check for unused CEs",
                       result['Message'])
         sys.exit(1)
-    
+
     result = updateSites(options.vo, result['Value'])
     if not result['OK']:
         gLogger.error("Error while updating sites", result['Message'])
+        sys.exit(1)
+
+    result = checkUnusedSEs(options.vo)
+    if not result['OK']:
+        gLogger.error("Error while running check for unused SEs",
+                      result['Message'])
+        sys.exit(1)
+
+    result = updateSEs(options.vo)
+    if not result['OK']:
+        gLogger.error("Error while updating SEs", result['Message'])
         sys.exit(1)
