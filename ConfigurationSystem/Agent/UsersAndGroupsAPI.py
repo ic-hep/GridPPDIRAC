8b1d506e296d67895097c970d21ffb82d5acbd4e
Alexander Richards <a.richards@imperial.ac.uk>
2015-02-04 13:52:08 +0000

    updates

--- ConfigurationSystem/Agent/UsersAndGroupsAPI.py
+++ ConfigurationSystem/Agent/UsersAndGroupsAPI.py
@@ -173,4 +173,4 @@ if __name__ == '__main__':
     ## for some reason config not loaded properly
     gConfig.loadFile('/opt/dirac/etc/DevelConfig.cfg')
     u=UsersAndGroupsAPI()
-    u.something()
+    u.update_usersandgroups()
