2afb7919c2888275fbb952b9bc548991d403893e
Simon Fayer <sf105@ic.ac.uk>
2017-05-11 14:34:26 +0100

    Set Platforms in JobRequirements to default as well.

--- WorkloadManagementSystem/Executor/JobPlatform.py
+++ WorkloadManagementSystem/Executor/JobPlatform.py
@@ -43,6 +43,13 @@ class JobPlatform( OptimizerExecutor ):
       # User didn't set platform, user default
       job_plat = def_plat
       manifest.setOption( "Platform", def_plat )
+      # We also have to set the platform in the job requirements
+      result = manifest.getSection( "JobRequirements" )
+      if not result[ 'OK' ]:
+        self.jobLog.error( "Failed to get job requirements." )
+        return S_ERROR( "Failed to get job requirements." ) 
+      requirements = result[ 'Value' ]
+      requirements.setOption( "Platforms", def_plat )
       self.jobLog.info( "Set job platform to default (%s)." % def_plat )
 
     return self.setNextOptimizer( jobState )
