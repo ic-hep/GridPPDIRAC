9c32c4077f602864ee06f6192ed750bb5cb9342d
Alexander Richards <a.richards@imperial.ac.uk>
2020-06-12 17:02:18 +0100

    Hot fix for issue https://github.com/ic-hep/DIRAC/issues/102

--- ConfigurationSystem/private/AutoResourceTools/ldaptools.py
+++ ConfigurationSystem/private/AutoResourceTools/ldaptools.py
@@ -46,8 +46,15 @@ class MockLdap(object):
         stdout = subprocess.check_output(shlex.split(cmd.format(host=self._host,
                                                                 base=base,
                                                                 filterstr=filterstr)))
-        return [(dn, dict(MockLdap.option_regex.findall(options)))
-                for dn, options in MockLdap.entry_regex.findall(stdout)]
+        ret = []
+        for dn, options in MockLdap.entry_regex.findall(stdout):
+            d = defaultdict(list)
+            for key, value in dict(MockLdap.option_regex.findall(options)):
+                d[key].append(value)
+            ret.append((dn, dict(d)))
+        return ret
+#        return [(dn, dict(MockLdap.option_regex.findall(options)))
+#                for dn, options in MockLdap.entry_regex.findall(stdout)]
 
 
 def in_(attrs, iterable):
