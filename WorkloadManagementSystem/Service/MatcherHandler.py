ee826c2fab2bf2f318198f110981377475780ea8
Daniela Bauer <dbauer@cern.ch>
2021-09-29 16:04:36 +0100

    modifications to accommodate v7r2

--- WorkloadManagementSystem/Service/MatcherHandler.py
+++ WorkloadManagementSystem/Service/MatcherHandler.py
@@ -3,7 +3,7 @@
     This provides a small wrapper around the DIRAC standard matcher,
     it allows re-writing of a node's dirac-platform to a more friendly name
     i.e. EL6 rather than Linux_x86_64_glibc-2.12.
-    The platforms are set via the GridPP_NodeCompatibility section in 
+    The platforms are set via the GridPP_NodeCompatibility section in
     Resources/Computing. A non-matched platform is kept unchanged.
 """
 
@@ -11,27 +11,21 @@ __RCSID__ = "$Id$"
 
 
 from DIRAC import gConfig, gLogger
-
-from DIRAC.WorkloadManagementSystem.Service.MatcherHandler import initializeMatcherHandler as initializeCoreMatcherHandler
 from DIRAC.WorkloadManagementSystem.Service.MatcherHandler import MatcherHandler as CoreMatcherHandler
 
 
-def initializeMatcherHandler( serviceInfo ):
-  return initializeCoreMatcherHandler( serviceInfo )
-
-
-class MatcherHandler( CoreMatcherHandler ):
+class MatcherHandler(CoreMatcherHandler):
   """ This class is just a thin wrapper around the main DIRAC Matcher service. """
-
-  def __patchPlatform( self, resourceDescription ):
+  @classmethod
+  def __patchPlatform(cls, resourceDescription):
     """ Replace the Platform field in a resource dictionary if it exists and
         a mapping exists in the DIRAC config system. """
 
     # Load a dict of the platforms to map
-    result = gConfig.getOptionsDict( '/Resources/Computing/GridPP_NodeCompatibility' )
+    result = gConfig.getOptionsDict('/Resources/Computing/GridPP_NodeCompatibility')
     if not result['OK']:
       # Fail to load dictionary
-      gLogger.error( "Failed to load NodeCompatibility" )
+      gLogger.error("Failed to load NodeCompatibility")
       return resourceDescription
     plats = result['Value']
 
@@ -46,19 +40,16 @@ class MatcherHandler( CoreMatcherHandler ):
 
     return resourceDescription
 
+  def export_requestJob(self, resourceDescription):
+    resourceDescription = self.__patchPlatform(resourceDescription)
+    return super(MatcherHandler, self).export_requestJob(resourceDescription)
 
-  def export_requestJob( self, resourceDescription ):
-    resourceDescription = self.__patchPlatform( resourceDescription )
-    return CoreMatcherHandler.export_requestJob( self, resourceDescription )
+  @classmethod
+  def export_getMatchingTaskQueues(cls, resourceDict):
+    resourceDict = cls.__patchPlatform(resourceDict)
+    return super(MatcherHandler, cls).export_getMatchingTaskQueues(resourceDict)
 
-
-  def export_getMatchingTaskQueues( self, resourceDict ):
-    resourceDict = self.__patchPlatform( resourceDict )
-    return CoreMatcherHandler.export_getMatchingTaskQueues( self, resourceDict )
-
-
-  def export_matchAndGetTaskQueue( self, resourceDict ):
-    resourceDict = self.__patchPlatform( resourceDict )
-    return CoreMatcherHandler.export_matchAndGetTaskQueue( self, resourceDict )
-
-
+  @classmethod
+  def export_matchAndGetTaskQueue(cls, resourceDict):
+    resourceDict = cls.__patchPlatform(resourceDict)
+    return super(MatcherHandler, cls).export_matchAndGetTaskQueue(resourceDict)
